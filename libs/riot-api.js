2f52eb16c172bcc745d8b4b4f144ae361d0e815a
unionyy <jadu1028@naver.com>
2021-02-02 01:19:39 +0900

    refactor: Make NormalReq & QueryReq, Generalize apis

--- libs/riot-api.js
+++ libs/riot-api.js
@@ -11,7 +11,7 @@ var config_global = {
     log     : false
 }
 
-function HttpsReq(_hostname, _path, _token) {
+function HttpsReq(_hostname, _path, _token, _log) {
     const options = {
         hostname: _hostname,
         port: 443,
@@ -24,12 +24,21 @@ function HttpsReq(_hostname, _path, _token) {
     };
     return new Promise((resolve, reject) => {
         const req = https.get(options, (res) => {
+            if(_log) {
+                console.log(`statusCode: ${res.statusCode}`);
+            }
             var _res = '';
             res.on('data', (d) => {
+                //process.stdout.write(d); // If you want to print res
                 _res += d;
             });
             res.on('end', () => {
-                resolve(JSON.parse(_res));
+                if(_res) {
+                    resolve(JSON.parse(_res));
+                } else {
+                    reject();
+                }
+                
             });
         });
         req.on('error', (error) => {
@@ -55,6 +64,7 @@ function MakeQueryParams(_params, PARAMS) {
     return query_params;
 }
 
+/* Make Overrided Configure */
 function MakeConfig(_config) {
     _config = _config || {};
 
@@ -67,6 +77,51 @@ function MakeConfig(_config) {
     return config_overided;
 }
 
+/* Request with only Path Prameter */
+function NormalReq(_path, _pathParam, _config) {
+    _config = MakeConfig(_config);
+    
+    var path = _path + urlencode.encode(_pathParam);
+
+    if(_config.log) {console.log('Get: ' + _config.platform + BASEURL + path);}
+
+    return HttpsReq(_config.platform + BASEURL, path, _config.token, _config.log);
+}
+
+/* Request with Path Prameter and Query Prameters */
+function QueryReq (_PATHPARAM, _path, _params, _config) {
+
+    if(typeof _params === 'string') {
+        return NormalReq(_path, _params, _config);
+    }
+
+    _config = MakeConfig(_config);
+    
+    var path = _path + urlencode.encode(_params[_PATHPARAM]);
+
+    var query_params = '';
+
+    for(param in _params) {
+        if(param === _PATHPARAM) {
+            continue;
+        } else {
+            if(query_params === '') {
+                query_params += '?';
+            }else {
+                query_params += '&';
+            }
+            query_params += param + '=';
+            query_params += urlencode.encode(_params[param]);
+        }
+    }
+
+    path += query_params;
+
+    if(_config.log) {console.log('Get: ' + _config.platform + BASEURL + path);}
+
+    return HttpsReq(_config.platform + BASEURL, path, _config.token, _config.log);
+}
+
 module.exports.SetGlobalConfig = function (_config) {
     config_global = MakeConfig(_config);
 
@@ -75,49 +130,30 @@ module.exports.SetGlobalConfig = function (_config) {
     }
 }
 
-module.exports.SummonerName = async function (name, _config) {
-    _config = MakeConfig(_config);
-
-    if(_config.log) {
-        console.log('Get Summoner by name: ' + name);
+module.exports.SummonerV4 = {
+    byAccount: function(encryptedAccountId, _config) {
+        return NormalReq('/lol/summoner/v4/summoners/by-account/', encryptedAccountId, _config);
+    },
+    byName: function(summonerName, _config) {
+        return NormalReq('/lol/summoner/v4/summoners/by-name/', summonerName, _config);
+    },
+    byPUUID: function(encryptedPUUID, _config) {
+        return NormalReq('/lol/summoner/v4/summoners/by-puuid/', encryptedPUUID, _config);
+    },
+    bySummonerId: function(encryptedSummonerId, _config) {
+        return NormalReq('/lol/summoner/v4/summoners/', encryptedSummonerId, _config);
     }
-    
-    var _path = '/lol/summoner/v4/summoners/by-name/' + urlencode.encode(name);
-    return HttpsReq(_config.platform + BASEURL, _path, _config.token);
 }
 
-module.exports.LeagueSummonerId = async function (encryptedSummonerId, _config) {
-    _config = MakeConfig(_config);
-
-    if(_config.log) {
-        console.log('Get League Entry by encryptedSummonerId: ' + encryptedSummonerId);
+module.exports.LeagueV4 = {
+    bySummoner: function(encryptedSummonerId, _config) {
+        return NormalReq('/lol/league/v4/entries/by-summoner/', encryptedSummonerId, _config);
     }
-    
-    var _path = '/lol/league/v4/entries/by-summoner/' + encryptedSummonerId;
-    return HttpsReq(_config.platform + BASEURL, _path, _config.token);
 }
 
-module.exports.MatchAccountId = async function (encryptedAccountId, _config) {
-    const PARAMS = ['champion', 'queue', 'season', 'endTime', 'beginTime', 'endIndex', 'beginIndex'];
-
-    _config = MakeConfig(_config);
-
-    var _params;
-    var query_params = '';
-
-    if(typeof encryptedAccountId === 'string') {
-        _params = {
-            encryptedAccountId: encryptedAccountId
-        }
-    } else {
-        _params = encryptedAccountId;
-        query_params = MakeQueryParams(_params, PARAMS);
+module.exports.MatchV4 = {
+    byAccount: function(encryptedAccountId, _config) {
+        return QueryReq('encryptedAccountId', '/lol/match/v4/matchlists/by-account/', encryptedAccountId, _config);
     }
-
-    if(_config.log) {
-        console.log('Get League Entry by encryptedAccountId: ' + _params.encryptedAccountId);
-    }
-    
-    var _path = '/lol/match/v4/matchlists/by-account/' + _params.encryptedAccountId + query_params;
-    return HttpsReq(_config.platform + BASEURL, _path, _config.token)  
 }
+
