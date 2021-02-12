039b37cdf630ca3834b0e86067ceedaef7e34355
unionyy <jadu1028@naver.com>
2021-02-13 01:09:03 +0900

    fix: Show logs along timestamp

--- docs/inittable.sql
+++ docs/inittable.sql
@@ -43,5 +43,5 @@ CREATE TABLE games (
     queue_type  smallint    NOT NULL,
     lane_my     tinyint     NOT NULL,
 
-    CONSTRAINT pk_my_game PRIMARY KEY (id_my, game_id DESC)
+    CONSTRAINT pk_my_time PRIMARY KEY (id_my, play_time DESC)
 );
