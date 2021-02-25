7109a67746b672bbba602377b78aaafe60e734eb
unionyy <jadu1028@naver.com>
2021-02-26 01:21:52 +0900

    fix: Remove Out of Date game log and rect

--- public/src/datecal.js
+++ public/src/datecal.js
@@ -41,40 +41,71 @@ for(date in pDate) {
     $('rect#rect-' + date).attr(pDate[date]);
 }
 
-/** Remove future rect */
-// -366
-var fakeTime = new Date(new Date() - 31622400000 - offset);
-var timeString = fakeTime.toISOString().slice(0, 10);
-$('rect#rect-' + timeString).css('display', 'none');
-$('a.log-' + timeString).attr('gameType', 'outofdate');
-
-// +1
-fakeTime = new Date(new Date() - (- 86400000) - offset);
-timeString = fakeTime.toISOString().slice(0, 10);
-$('rect#rect-' + timeString).css('display', 'none');
-
-
 /** Udpate period */
 var endStr = $('span#username-period-end').text();
 var beginStr = $('span#username-period-begin').text();
 var end;
 var begin;
 if(endStr === '???') {
-    end = (new Date(new Date() - 31536000000 - offset)).toISOString().slice(0, 10);
+    end = (new Date(new Date() - 31536000000 - offset))
 } else {
-    end = (new Date(endStr * 1000)).toISOString().slice(0, 10);
+    end = (new Date(endStr * 1000))
 }
 if(beginStr === '???') {
-    begin = (new Date(new Date() - offset)).toISOString().slice(0, 10);
+    begin = (new Date(new Date() - offset))
 } else {
-    begin = (new Date(beginStr * 1000)).toISOString().slice(0, 10);
+    begin = (new Date(beginStr * 1000))
 }
 
-$('#username-period-end').text(end);
-$('#username-period-begin').text(begin);
+endStr = end.toISOString().slice(0, 10);
+beginStr = begin.toISOString().slice(0, 10);
+
+/** Remove out of date rect, remove count*/
+// -1
+var timeStr = new Date(end - 86400000).toISOString().slice(0, 10);
+$('rect#rect-' + timeStr).css('display', 'none');
+$('rect#rect-' + timeStr).attr({
+    "data-count-total": 0,
+    "data-count-solo" : 0, // 솔랭
+    "data-count-flex" : 0, // 자랭
+    "data-count-norm" : 0, // 일반
+    "data-count-aram" : 0, // 칼바람
+    "data-count-urf"  : 0, // 우루프
+    "data-count-ai"   : 0, // 봇전
+    "data-count-ofa"  : 0, // 
+    "data-count-nbg"  : 0,
+    "data-count-tut"  : 0,
+    "data-count-clash"  : 0,
+    "data-count-poro"  : 0,
+    "data-count-etc"  : 0
+});
+$('a.log-' + timeStr).attr('gameType', 'outofdate');
+
+// +1
+var timeStr = new Date(begin - (-86400000)).toISOString().slice(0, 10);
+$('rect#rect-' + timeStr).css('display', 'none');
+$('rect#rect-' + timeStr).attr({
+    "data-count-total": 0,
+    "data-count-solo" : 0, // 솔랭
+    "data-count-flex" : 0, // 자랭
+    "data-count-norm" : 0, // 일반
+    "data-count-aram" : 0, // 칼바람
+    "data-count-urf"  : 0, // 우루프
+    "data-count-ai"   : 0, // 봇전
+    "data-count-ofa"  : 0, // 
+    "data-count-nbg"  : 0,
+    "data-count-tut"  : 0,
+    "data-count-clash"  : 0,
+    "data-count-poro"  : 0,
+    "data-count-etc"  : 0
+});
+$('a.log-' + timeStr).attr('gameType', 'outofdate');
+
+$('#username-period-end').text(endStr);
+$('#username-period-begin').text(beginStr);
 
 /** Period Selector Setting */
-$('#period-selector-begin').attr('value', begin);
-$('#period-selector-end').attr('value', end);
+$('#period-selector-begin').attr('value', beginStr);
+$('#period-selector-end').attr('value', endStr);
 $('#period-selector-begin').attr('max', (new Date(new Date() - offset)).toISOString().slice(0, 10));
 $('#period-selector-end').attr('max', (new Date(new Date() - offset)).toISOString().slice(0, 10));
