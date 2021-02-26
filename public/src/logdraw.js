d7243d7bf2fcb85dda95212fd44cc742ec2e552c
unionyy <jadu1028@naver.com>
2021-02-26 16:06:57 +0900

    refactor: Make log html at client side

---
+++ public/src/logdraw.js
@@ -0,0 +1,20 @@
+DrawLog = function(RIOTCDNURI) {
+    const CHAMPIONURI = RIOTCDNURI + '/img/champion/';
+    const array = JSON.parse($('#user-games-body').html())['data'];
+    var html = '';
+
+    for(elem of array) {
+        //var url = `/${Object.keys(PLATFORM_MY)[game.platform_my]}/match/${game.game_id}`;
+        var url = `https://matchhistory.kr.leagueoflegends.com/#match-details/${elem[0]}?tab=overview`;
+        html +=
+            `<a class="user-games-game" href="${url}" target="_blank" rel="noopener noreferrer" timestamp="${elem[1]}" gametype="${elem[2]}" champname="${elem[3]}" champid="${elem[3]}" lane="${elem[4]}">
+                <img class="user-games-icon" src="${CHAMPIONURI + elem[3]}.png">
+                <span class="user-games-date">???</span><br>
+                <span class="user-games-type">${elem[2]}</span>
+            </a>
+            `;
+    }
+
+    $('#user-games-body').html(html);
+    
+}
