298852b488fa73ce2137d1ec5185f236de72e254
unionyy <jadu1028@naver.com>
2021-03-03 02:51:39 +0900

    feat: Hide settings on mobile

--- public/src/hidelog.js
+++ public/src/hidelog.js
@@ -1,11 +1,11 @@
 function HideLog() {
     $('#user-games-body').attr('hidden', true);
-    $('.fa-caret-up').css('display', 'none');
-    $('.fa-caret-down').css('display', 'inline');
+    $('#user-games-header .fa-caret-up').css('display', 'none');
+    $('#user-games-header .fa-caret-down').css('display', 'inline');
 }
 
 function ShowLog() {
     $('#user-games-body').attr('hidden', false);
-    $('.fa-caret-up').css('display', 'inline');
-    $('.fa-caret-down').css('display', 'none');
+    $('#user-games-header .fa-caret-up').css('display', 'inline');
+    $('#user-games-header .fa-caret-down').css('display', 'none');
 }
