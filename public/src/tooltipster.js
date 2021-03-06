a8fdfc2ff331e8adb7aa7f8ed977aee323b510df
unionyy <jadu1028@naver.com>
2021-03-06 11:39:00 +0900

    refactor: Seperate inline script to individual js file

---
+++ public/src/tooltipster.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('.day').tooltipster({
+        theme: 'tooltipster-borderless',
+        plugins: ['follower'],
+        anchor: 'bottom-center',
+        offset: [0, 10],
+        arrow: true
+    });
+    $('.user-games-game').tooltipster({
+        theme: 'tooltipster-borderless',
+        plugins: ['follower'],
+        anchor: 'bottom-center',
+        offset: [0, 10],
+        arrow: true
+    });
+});
