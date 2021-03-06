f4d379c5838da364cf0f04b3a2a280f1cfb69df8
unionyy <jadu1028@naver.com>
2021-03-06 13:31:55 +0900

    Add Google tag preview CSP

--- lologme.js
+++ lologme.js
@@ -40,9 +40,11 @@ const cspOptions = {
   directives: {
     ...helmet.contentSecurityPolicy.getDefaultDirectives(),
     "script-src": ["'self'", "*.googleapis.com",
-              "https://www.googletagmanager.com", "https://www.google-analytics.com", "https://ssl.google-analytics.com",
+              "https://www.googletagmanager.com", "https://www.google-analytics.com", "https://ssl.google-analytics.com", "https://tagmanager.google.com",
               "*.gstatic.com", "'unsafe-inline'"],
-    "img-src": ["'self'", "*.leagueoflegends.com", "www.googletagmanager.com", "https://www.google-analytics.com"],
+    "style-src": ["https://tagmanager.google.com", "https://fonts.googleapis.com"],
+    "img-src": ["'self'", "*.leagueoflegends.com", "www.googletagmanager.com", "https://www.google-analytics.com" , "https://ssl.gstatic.com", "https://www.gstatic.com"],
+    "font-src": ["https://fonts.gstatic.com", "data:"],
     "connect-src": ["'self'", "https://www.google-analytics.com"]
   }
 }
