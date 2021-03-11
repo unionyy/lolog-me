2e917776efa26f877c6614c4767c7e4c2f1849b5
unionyy <jadu1028@naver.com>
2021-03-12 00:50:48 +0900

    feat: Update Game Detail when search game

--- docs/updategames.sql
+++ docs/updategames.sql
@@ -1,4 +1,4 @@
-ALTER TABLE games ADD wins tinyint default -1;
+ALTER TABLE games ADD win_my tinyint default -1;
 ALTER TABLE games ADD kills smallint default -1;
 ALTER TABLE games ADD deaths smallint default -1;
 ALTER TABLE games ADD assists smallint default -1;
