39a15522a29232d2c094b7df99237b796bb27734
unionyy <jadu1028@naver.com>
2021-03-13 18:08:49 +0900

    feat: CSSing for match

--- public/src/matchdraw.js
+++ public/src/matchdraw.js
@@ -34,42 +34,58 @@ async function GetMatch(_matchId, _platform, _timestamp) {
 
                     var partHtml = `<li class="team-part">
 <div class="part-champ cell">
-    <div class="part-icon">
-        <img src="${RIOTCDNURI+VERSION.latest}/img/champion/${CHAMPION[elem.champ]}.png" alt="${CHAMPION[elem.champ]}" title="${CHAMPION[elem.championId]}" />
-        <span>${elem.stats.champLevel}</span>
-    </div>
-    <div class="part-spell">
-        <img src="${RIOTCDNURI+VERSION.latest}/img/spell/${SPELL[elem.spell1Id]}.png" />
-        <img src="${RIOTCDNURI+VERSION.latest}/img/spell/${SPELL[elem.spell2Id]}.png" />
-    </div>
-    <div class="part-rune">
-        <img src="https://ddragon.leagueoflegends.com/cdn/img/${RUNE[elem.stats.rune0]}" />
-        <img src="https://ddragon.leagueoflegends.com/cdn/img/${RUNE[elem.stats.rune1]}" />
+    <div class="inner-cell">
+        <div class="part-rune">
+            <img class="rune-main" src="https://ddragon.leagueoflegends.com/cdn/img/${RUNE[elem.stats.rune0]}" />
+            <img class="rune-sub" src="https://ddragon.leagueoflegends.com/cdn/img/${RUNE[elem.stats.rune1]}" />
+        </div>
+        <div class="part-spell">
+            <img src="${RIOTCDNURI+VERSION.latest}/img/spell/${SPELL[elem.spell1Id]}.png" />
+            <img src="${RIOTCDNURI+VERSION.latest}/img/spell/${SPELL[elem.spell2Id]}.png" />
+        </div>
+        <div class="part-level">
+            <span>${elem.stats.champLevel}</span>     
+        </div>
     </div>
 </div>
 <div class="part-name cell">
-    <span>${elem.id.name}</span>
+    <div class="inner-cell">
+        <div class="part-icon">
+            <img src="${RIOTCDNURI+VERSION.latest}/img/champion/${CHAMPION[elem.champ]}.png" alt="${CHAMPION[elem.champ]}" title="${CHAMPION[elem.championId]}" />
+        </div>
+        <span>${elem.id.name}</span>
+    </div>
 </div>
 <div class="part-item cell">
-    <img src="${cdnuri}/img/item/${elem.stats.item0}.png" />
-    <img src="${cdnuri}/img/item/${elem.stats.item1}.png" />
-    <img src="${cdnuri}/img/item/${elem.stats.item2}.png" />
-    <img src="${cdnuri}/img/item/${elem.stats.item3}.png" />
-    <img src="${cdnuri}/img/item/${elem.stats.item4}.png" />
-    <img src="${cdnuri}/img/item/${elem.stats.item5}.png" />
-    <img src="${cdnuri}/img/item/${elem.stats.item6}.png" />
+    <div class="inner-cell">
+        <img src="${cdnuri}/img/item/${elem.stats.item0}.png" />
+        <img src="${cdnuri}/img/item/${elem.stats.item1}.png" />
+        <img src="${cdnuri}/img/item/${elem.stats.item2}.png" />
+        <img src="${cdnuri}/img/item/${elem.stats.item3}.png" />
+        <img src="${cdnuri}/img/item/${elem.stats.item4}.png" />
+        <img src="${cdnuri}/img/item/${elem.stats.item5}.png" />
+        <img src="${cdnuri}/img/item/${elem.stats.item6}.png" />
+    </div>
 </div>
 <div class="part-kda cell">
-    <span>${elem.stats.kills}/${elem.stats.deaths}/${elem.stats.assists} (${Math.ceil((elem.stats.kills + elem.stats.assists) / data[team].kills * 100)}%)</span>
+    <div class="inner-cell">
+        <span>${elem.stats.kills}/${elem.stats.deaths}/${elem.stats.assists} (${Math.ceil((elem.stats.kills + elem.stats.assists) / data[team].kills * 100)}%)</span>
+    </div>
 </div>
 <div class="part-cs cell">
-    <span title="${elem.stats.minions} + ${elem.stats.jungle}">${elem.stats.minions + elem.stats.jungle}</span>
+    <div class="inner-cell">
+        <span title="${elem.stats.minions} + ${elem.stats.jungle}">${elem.stats.minions + elem.stats.jungle}</span>
+    </div>
 </div>
 <div class="part-gold cell">
-    <span>${elem.stats.gold}</span>
+    <div class="inner-cell">
+        <span>${elem.stats.gold}</span>
+    </div>
 </div>
 <div class="part-damage cell">
-    <span title="${elem.stats.deal}/${elem.stats.dealTotal}">${elem.stats.deal}</span>
+    <div class="inner-cell">
+        <span title="${elem.stats.deal}/${elem.stats.dealTotal}">${elem.stats.deal}</span>
+    </div>
 </div>
 </li>`;
                     teamHtml += partHtml;
