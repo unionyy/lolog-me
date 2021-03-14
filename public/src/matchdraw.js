573b76d480e22c658caf123d23279d45b20ed15b
unionyy <jadu1028@naver.com>
2021-03-14 23:53:27 +0900

    feat: Add icon & highlight user

--- public/src/matchdraw.js
+++ public/src/matchdraw.js
@@ -37,9 +37,9 @@ async function GetMatch(_matchId, _platform, _timestamp) {
                 if(data.teams[team].win < 10) {
                     teamText = 'Unkown';
                 } else if(data.teams[team].win < 20) {
-                    teamText = 'BlueTeam';
+                    teamText = 'Blue';
                 } else {
-                    teamText = 'RedTeam';
+                    teamText = 'Red';
                 }
 
                 var teamHtml = `
@@ -56,43 +56,56 @@ async function GetMatch(_matchId, _platform, _timestamp) {
                                 <span class="text-kda-header text-color-${winText}">${data.teams[team].kills}</span><span class="text-color-${winText}">/</span>
                                 <span class="text-kda-header text-color-${winText}">${data.teams[team].deaths}</span><span class="text-color-${winText}">/</span>
                                 <span class="text-kda-header text-color-${winText}">${data.teams[team].assists}</span>
+                                <img class="icon-header ${winText}" src="/images/icon/mask-icon-offense.png" />
                             </div>
                         </div>
                         <div class="col-item cell">
                             <div class="inner-cell-header">
-                                ${data.teams[team].gold}
+                                <span class="text-gold-header">${data.teams[team].gold.toLocaleString('ko-KR')}</span>
+                                <img class="icon-header" src="/images/icon/mask-icon-gold.png" />
                             </div>
                         </div>
                         <div class="col-kda cell">
                             <div class="inner-cell-header">
-                                KDA
+                            <img class="icon-header" src="/images/icon/mask-icon-offense.png" />
                             </div>
                         </div>
-                        <div class="col-cs cell"><div class="inner-cell-header">CS</div></div>
-                        <div class="col-gold cell"><div class="inner-cell-header">Gold</div></div>
+                        <div class="col-cs cell"><div class="inner-cell-header">
+                            <img class="icon-header" src="/images/icon/mask-icon-cs.png" />
+                        </div></div>
+                        <div class="col-gold cell"><div class="inner-cell-header">
+                            <img class="icon-header" src="/images/icon/mask-icon-gold.png" />
+                        </div></div>
                         <div class="col-damage cell"><div class="inner-cell-header">Damage</div></div>
                     </header>
                     <ul class="team-container">`;
                 for (elem of data.teams[team].participants) {
                     /** Current User */
+                    var isMe = '';
                     if($('#user-profile-name').attr('accountId') === elem.id.accountId) {
                         myTeam = team;
+                        isMe = ' is-me';
                     }
 
                     /** Item Images */
                     var itemsHtml = '';
-                    for(item of elem.stats.items) {
-                        if(item === 0) {
-                            itemsHtml += '<img />'
+                    var classStr = 'item';
+                    for(i in elem.stats.items) {
+                        var classStrCur = classStr;
+                        if(i === '0') {
+                            classStrCur += ' item-first';
+                        }
+                        if(elem.stats.items[i] === 0) {
+                            itemsHtml += `<rect class="${classStrCur}"></rect>`;
                         } else {
-                            itemsHtml += `<img src="${cdnuri}/img/item/${item}.png" />`
+                            itemsHtml += `<img class="${classStrCur}" src="${cdnuri}/img/item/${elem.stats.items[i]}.png" />`
                         }
                     }
                     /** Kill Participation */
                     var killPart = 0;
                     if(data.teams[team].kills) killPart = Math.ceil((elem.stats.kills + elem.stats.assists) / data.teams[team].kills * 100);
 
-                    var partHtml = `<li class="team-part">
+                    var partHtml = `<li class="team-part${isMe}">
                         <div class="part-champ cell">
                             <div class="inner-cell">
                                 <div class="part-rune">
@@ -100,8 +113,8 @@ async function GetMatch(_matchId, _platform, _timestamp) {
                                     <img class="rune-sub" src="https://ddragon.leagueoflegends.com/cdn/img/${RUNE[elem.stats.rune1]}" />
                                 </div>
                                 <div class="part-spell">
-                                    <img src="${RIOTCDNURI + VERSION.latest}/img/spell/${SPELL[elem.spell1Id]}.png" />
-                                    <img src="${RIOTCDNURI + VERSION.latest}/img/spell/${SPELL[elem.spell2Id]}.png" />
+                                    <img class="spell1" src="${RIOTCDNURI + VERSION.latest}/img/spell/${SPELL[elem.spell1Id]}.png" />
+                                    <img class="spell2" src="${RIOTCDNURI + VERSION.latest}/img/spell/${SPELL[elem.spell2Id]}.png" />
                                 </div>
                                 <div class="part-level">
                                     <span>${elem.stats.champLevel}</span>     
@@ -112,6 +125,7 @@ async function GetMatch(_matchId, _platform, _timestamp) {
                             <div class="inner-cell">
                                 <div class="part-icon">
                                     <img src="${RIOTCDNURI + VERSION.latest}/img/champion/${CHAMPION[elem.champ]}.png" alt="${CHAMPION[elem.champ]}" title="${CHAMPION[elem.championId]}" />
+                                    <div class="shadow"></div>
                                 </div>
                                 <span>${elem.id.name}</span>
                             </div>
