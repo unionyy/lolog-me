5d85b20eb520edc41a5db8f74b392b0cb107c9c1
unionyy <jadu1028@naver.com>
2021-03-18 00:10:36 +0900

    feat: Send log array rather than object 1/3 size of text

--- public_full/src/logdraw.js
+++ public_full/src/logdraw.js
@@ -3,12 +3,18 @@ DrawLog = function() {
     var html = '';
 
     for(elem of logArray) {
+        const platform = elem[0];
+        const matchId = elem[1];
+        const timestamp = elem[2];
+        const queue = elem[3];
+        const champion = elem[4];
+        const lane = elem[5];
         html +=
-            `<div class="user-games-game" platform="${elem.platform}" matchId="${elem.matchId}" timestamp="${elem.timestamp}" gametype="${elem.queue}" champname="${elem.champion}" champid="${elem.champion}" lane="${elem.lane}">
+            `<div class="user-games-game" platform="${platform}" matchId="${matchId}" timestamp="${timestamp}" gametype="${queue}" champname="${champion}" champid="${champion}" lane="${lane}">
                 <a class="user-games-mini">
-                    <img class="user-games-icon" src="${CHAMPIONURI + CHAMPION[elem.champion]}.png" alt="${CHAMPION[elem.champion]}">
+                    <img class="user-games-icon" src="${CHAMPIONURI + CHAMPION[champion]}.png" alt="${CHAMPION[champion]}">
                     <span class="user-games-date">???</span><br>
-                    <span class="user-games-type">${LANG['icon_'+elem.queue]}</span>
+                    <span class="user-games-type">${LANG['icon_'+ queue]}</span>
                 </a>
             </div>
             `;
