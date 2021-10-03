6e15d6ec4e2e4e67a08328268658a2bc88cf95be
unionyy <jadu1028@naver.com>
2021-10-04 00:29:47 +0900

    move public_full to public

--- public/src/hidesetting.js
+++ public/src/hidesetting.js
@@ -1,1 +1,13 @@
-function HideSetting(){$('#type-check').css('display','none');$('#period').css('display','none');$('#user-graph-header .fa-caret-up').css('display','none');$('#user-graph-header .fa-caret-down').css('display','inline')}function ShowSetting(){$('#type-check').css('display','grid');$('#period').css('display','block');$('#user-graph-header .fa-caret-up').css('display','inline');$('#user-graph-header .fa-caret-down').css('display','none')}
+function HideSetting() {
+    $('#type-check').css('display', 'none');
+    $('#period').css('display', 'none');
+    $('#user-graph-header .fa-caret-up').css('display', 'none');
+    $('#user-graph-header .fa-caret-down').css('display', 'inline');
+}
+
+function ShowSetting() {
+    $('#type-check').css('display', 'grid');
+    $('#period').css('display', 'block');
+    $('#user-graph-header .fa-caret-up').css('display', 'inline');
+    $('#user-graph-header .fa-caret-down').css('display', 'none');
+}
